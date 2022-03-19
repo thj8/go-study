a0cd05be0ba4cb8b83966034bf9d4d5d7e7eab1b
tanghaijun <tanghj@cyberpeace.cn>
2022-03-19 23:16:54 +0800

    part5-2 change print blockchain detail info

--- blockchain/btc/transaction.go
+++ blockchain/btc/transaction.go
@@ -11,6 +11,7 @@ import (
 	"fmt"
 	"log"
 	"math/big"
+	"strings"
 )
 
 const subidy = 10
@@ -52,6 +53,30 @@ func (tx *Transaction) Seralize() []byte {
 	}
 
 	return encoded.Bytes()
+}
+
+// String returns a human-readable representation of a transaction
+func (tx Transaction) String() string {
+	var lines []string
+
+	lines = append(lines, fmt.Sprintf("--- Transaction %x:", tx.ID))
+
+	for i, input := range tx.Vin {
+
+		lines = append(lines, fmt.Sprintf("     Input %d:", i))
+		lines = append(lines, fmt.Sprintf("       TXID:      %x", input.Txid))
+		lines = append(lines, fmt.Sprintf("       Out:       %d", input.Vout))
+		lines = append(lines, fmt.Sprintf("       Signature: %x", input.Signature))
+		lines = append(lines, fmt.Sprintf("       PubKey:    %x", input.PubKey))
+	}
+
+	for i, output := range tx.Vout {
+		lines = append(lines, fmt.Sprintf("     Output %d:", i))
+		lines = append(lines, fmt.Sprintf("       Value:  %d", output.Value))
+		lines = append(lines, fmt.Sprintf("       PubKeyHash: %x", output.PubKeyHash))
+	}
+
+	return strings.Join(lines, "\n")
 }
 
 // Hash returns the hash of the Transaction
